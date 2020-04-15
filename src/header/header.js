5c241aadca69951ad30af2543e2ae9a83402bd8a
Taylor Piccarreto <tpic12@gmail.com>
2020-04-15 13:14:33 -0400

    burger menu

--- src/header/header.js
+++ src/header/header.js
@@ -6,14 +6,29 @@ const Header = ({ sticky }) => {
     return "{tP}";
   };
   return (
-    <nav className={sticky ? "navbar navbar-sticky" : "navbar navbar-hidden"}>
+    <nav
+      role="navigation"
+      className={sticky ? "navbar navbar-sticky" : "navbar navbar-hidden"}
+    >
+      <div id="menuToggle">
+        <input type="checkbox" />
+        <span></span>
+        <span></span>
+        <span></span>
+        <ul id="menu">
+          <a href="#">
+            <li>About</li>
+          </a>
+          <a href="#">
+            <li>Projects</li>
+          </a>
+          <a href="#">
+            <li>Contact</li>
+          </a>
+        </ul>
+      </div>
       <div className="navbar--logo-holder">
         <h1>{name()}</h1>
-      </div>
-      <div className="navbar--link">
-        <div className="navbar--link-item">About</div>
-        <div className="navbar--link-item">Projects</div>
-        <div className="navbar--link-item">Contact</div>
       </div>
     </nav>
   );
