2930fcf29d6aa8c22ca915b231ee7e137fc28d65
Taylor Piccarreto <tpic12@gmail.com>
2020-04-23 15:07:14 -0400

    landing/about css

--- src/About/About.js
+++ src/About/About.js
@@ -5,8 +5,9 @@ import "./About.css";
 class About extends Component {
   render() {
     return (
-      <section id="about-section" className="about-section">
-        <div className="about-me snap">
+      <section id="about-section" className="about-section snap">
+        <div className="header-space"></div>
+        <div className="about-me">
           <p>
             {" "}
             &emsp; A NYC transplant from upstate New York, I came here for a job
@@ -17,14 +18,21 @@ class About extends Component {
             <br />
             <br />
             &emsp;I have always enjoyed solving problems, from how to add
-            different parts to my computer to rock climbing. Looking at
-            something and figuring out how to accomplish what looks impossible
-            drives me to try, and then when it gets done, to do it better. When
-            a good challenge comes along, I pride my self on my adaptability,
-            being in numerous work industries (hospitality, to Wall Street, to
-            Development) I have learned to take skills acquired and apply them
-            in non-conventional ways, to view a problem from my point of view,
-            the Taylor v4.0 view.
+            different parts to my computer to rock climbing. Just like in rock
+            climbing, something can be developed in more than one way, but it’s
+            all about finding the most efficient way for your product.
+            Frameworks or languages that work for one team may not be best for
+            another, even though they both get the job done. That's why teamwork
+            is also one of my major cornerstones, having those diverse points of
+            view to work with daily can open up your eyes to alternative methods
+            that beofre were completely lost to you.
+            <br />
+            <br />
+            When a good challenge comes along, I pride my self on my
+            adaptability, being in numerous work industries (hospitality, to
+            Wall Street, to Development) I have learned to take skills acquired
+            and apply them in non-conventional ways, to view a problem from my
+            point of view, the Taylor v4.0 view.
           </p>
           <div className="about-circle">
             <img src={Pic} className="pic" alt="taylor" />
--- src/Landing/Landing.js
+++ src/Landing/Landing.js
@@ -16,19 +16,21 @@ const Landing = ({ stickyRef }) => {
     <>
       <section className="landing-section snap">
         <div className="landing-div" ref={stickyRef}>
-          <div className="name-tag-div">
-            <h1>
-              <span className="thin">Taylor</span>
-              <br />
-              <span className="bold">Piccarreto</span>
-            </h1>
-            <p>
-              Hi, I’m Taylor, and I like my applications like I like my
-              pancakes: Fullstack and no bugs.
-            </p>
-          </div>
-          <div className="landing-circle">
-            <span className="logo">{logo()}</span>
+          <div className="landing-content">
+            <div className="name-tag-div">
+              <h1>
+                <span className="thin">Taylor</span>
+                <br />
+                <span className="bold">Piccarreto</span>
+              </h1>
+              <p>
+                Hi, I’m Taylor, and I like my applications like I like my
+                pancakes: Fullstack and no bugs.
+              </p>
+            </div>
+            <div className="landing-circle">
+              <span className="logo">{logo()}</span>
+            </div>
           </div>
           <FontAwesomeIcon icon="chevron-down" size="lg" />
         </div>
